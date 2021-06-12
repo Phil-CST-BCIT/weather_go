b33a57e66eab97b1f49c12063b3d000db07bd071
Phil <philfeiteng@gmail.com>
2021-06-12 00:11:46 -0700

    add latest

--- weather/weather.go
+++ weather/weather.go
@@ -1,8 +1,12 @@
 package weather
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
 )
 
 // 1. read the api key from apikey.txt
@@ -10,9 +14,18 @@ import (
 // 3. pass the newly created url to http.Get method to get the response
 // 4. parse the response
 // 5. compute the temperature
-// 6.
 
-func Apikey() string {
+type Report struct {
+	Name        string  `json:"name"`
+	Country     string  `json:"country"`
+	Temp        float64 `json:"temp"`
+	Humidity    float64 `json:"humidity"`
+	Description string  `json:"description"`
+}
+
+// read the api key from a txt file
+// return the api key as a string
+func apikey() string {
 	b, err := ioutil.ReadFile("../apikey.txt")
 
 	if err != nil {
@@ -21,7 +34,147 @@ func Apikey() string {
 
 	str := string(b)
 
-	// fmt.Println(string(b))
-
 	return str
+}
+
+// insert a location into the url query for a GET request
+func url_for(location string) string {
+
+	api_key := apikey()
+
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric", location, api_key)
+
+	return url
+}
+
+// retrieve the weather information from openwehthermap
+// return the response body, which is a slice of bytes
+func get_weather_info(url string) []byte {
+
+	res, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	body, e := io.ReadAll(res.Body)
+
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	return body
+
+}
+
+// obtain the desired data from the byte slice
+func parse(b []byte) *Report {
+
+	// we first declare an interface for storing the decoded json data
+	var intf interface{}
+
+	// encoding/json provides the capability to parse arbitrary JSON into an interface
+	// parse the json and store it into intf
+	err := json.Unmarshal(b, &intf)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := new(Report)
+
+	parse_helper(intf, r)
+
+	return r
+}
+
+// we have a go type interface, which means it can be anything
+// we want to extract data from the interface and assign
+// the data to the corresponding fields of Report
+// we need to recursively walk through the data structure
+func parse_helper(intf interface{}, r *Report) {
+
+	// first, we check the type of the interface
+	switch v := intf.(type) {
+
+	// if it is of type array
+	case []interface{}:
+
+		// take each element in the array
+		for _, u := range v {
+			parse_helper(u, r)
+		}
+
+	// if it's of type map of string to anything
+	case map[string]interface{}:
+
+		if v["name"] != nil {
+
+			// In order to assign the value to a field of Report
+			// we need to check the concrete type of map v of string to something
+			// if it is a map of string to string
+			if str, ok := v["name"].(string); ok {
+				r.Name = str
+			}
+		}
+
+		if v["country"] != nil {
+
+			if str, ok := v["country"].(string); ok {
+				r.Country = str
+			}
+		}
+
+		if v["humidity"] != nil {
+
+			if fl, ok := v["humidity"].(float64); ok {
+				r.Humidity = fl
+			}
+		}
+
+		if v["temp"] != nil {
+
+			if fl, ok := v["temp"].(float64); ok {
+				r.Temp = fl
+			}
+		}
+
+		if v["description"] != nil {
+
+			if str, ok := v["description"].(string); ok {
+				r.Description = str
+			}
+		}
+
+		// get a vale from the map and use the value to find fields we need
+		// until every value in the map is parsed
+		for _, w := range v {
+			parse_helper(w, r)
+		}
+	}
+}
+
+// for second solution
+// func pretty_print(r *Report) {
+
+// 	str := fmt.Sprintf("%s %s: %.2f deg C, %d%%, %s", r.Name, r.Country, r.Temp, int64(r.Humidity), r.Description)
+
+// 	fmt.Println(str)
+
+// }
+
+func Temperature_of(location string, ch chan *Report) {
+
+	url := url_for(location)
+
+	b := get_weather_info(url)
+
+	r := parse(b)
+
+	ch <- r
+
+	// pretty_print(r)
+
 }
--- wthr/wthr.go
+++ wthr/wthr.go
@@ -2,10 +2,158 @@ package main
 
 import (
 	"fmt"
-    "localhost/weather"
+	"localhost/weather"
+	"os"
+	"sort"
+	"sync"
 )
 
+var (
+	wg sync.WaitGroup
+)
+
+// we can delete the Report type and its methods
+// if using the second way for solving this problem
+type Report struct {
+	Name        string
+	Country     string
+	Temp        float64
+	Humidity    float64
+	Description string
+}
+
+type ByName []Report
+
+func (rs ByName) Len() int {
+	return len(rs)
+}
+
+func (rs ByName) Swap(i, j int) {
+	rs[i], rs[j] = rs[j], rs[i]
+}
+
+func (rs ByName) Less(i, j int) bool {
+	return rs[i].Name < rs[j].Name
+}
+
+// get cmd line arguments and return a string slice
+func collect_cities() []string {
+
+	args := os.Args[1:]
+
+	cities := []string{}
+
+	cities = append(cities, args...)
+
+	return cities
+}
+
+// many goroutines will call this function
+// we want to create a critical section for sychronization
+// if using the second solution
+func city_weather(city string, ch chan *weather.Report) {
+
+	defer wg.Done()
+
+	weather.Temperature_of(city, ch)
+
+}
+
+func create_report(wr *weather.Report) *Report {
+
+	r := new(Report)
+
+	r.Name = wr.Name
+	r.Country = wr.Country
+	r.Temp = wr.Temp
+	r.Humidity = wr.Humidity
+	r.Description = wr.Description
+
+	return r
+}
+
+func my_printer(rs []Report, n int) {
+
+	for _, v := range rs {
+
+		str := fmt.Sprintf("%s %s: %.2f deg C, %d%%, %s", v.Name, v.Country, v.Temp, int64(v.Humidity), v.Description)
+
+		fmt.Println(str)
+
+	}
+
+	if n > 0 {
+		fmt.Fprintf(os.Stderr, "Number of incorrect city names: %d\n", n)
+	}
+
+}
+
 func main() {
-    k := weather.Apikey()
-	fmt.Println(k)
+
+	cities := collect_cities()
+
+	rs := []Report{}
+
+	var counter int
+
+	wg.Add(len(cities))
+
+	// From Albert
+	// creating a go routine & immediately reading from the channel
+	// associated with the go routine makes it sequential
+	for _, v := range cities {
+
+		ch := make(chan *weather.Report)
+
+		go city_weather(v, ch)
+
+		wr := <-ch
+
+		var r *Report
+
+		if len(wr.Name) > 0 {
+			r = create_report(wr)
+			rs = append(rs, *r)
+		} else {
+			counter++
+		}
+
+	}
+
+	sort.Sort(ByName(rs))
+
+	my_printer(rs, counter)
+
+	// THE FOLLOWING CODE IS THE SECOND SOLUTION
+	// We first collect all the cities from command line
+	// cities is a string slice, so we can sort it
+	// after sorting the slice, we can use channel or mutex for synchronization
+	// the result then is printed out in weather.go
+
+	// cities := collect_cities()
+
+	// sort.Strings(cities)
+
+	// ch := make(chan bool, 1)
+
+	// lock := make(chan bool, 1)
+
+	// for _, v := range cities {
+
+	// 	// the main goroutine is blocked here until the New York goroutine unlocks
+	// 	lock <- true
+
+	// 	// the first city is New York City
+	// 	go city_weather(v, ch, lock)
+
+	// 	// the main goroutine sends the first message to the channel
+	// 	ch <- true
+
+	// }
+
+	// close(ch)
+	// close(lock)
+
+	wg.Wait()
+
 }
